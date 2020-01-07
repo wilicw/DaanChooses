8bdbc8d90d8074df3c12b0936383a8b85e262816
William Chang <wilicw1346@gmail.com>
2020-01-07 19:59:33 +0800

    Author url

--- cli/src/Components/Footer.vue
+++ cli/src/Components/Footer.vue
@@ -3,7 +3,7 @@
   <v-footer class="text-center pa-0 ma-0">
     <v-card flat tile color="#fafafa" width="100vw" class="text-center">
       <v-card-text class="orange--text">
-        Made by <a target="_blank" rel="noopener noreferrer" href="https://github.com/daancsc/DaanChoose-Flask">@wilicw</a>. Source Code on <a target="_blank" rel="noopener noreferrer" href="https://github.com/daancsc/DaanChoose-Flask"><strong>Github</strong></a><br>
+        Made by <a target="_blank" rel="noopener noreferrer" href="https://github.com/wilicw">@wilicw</a>. Source Code on <a target="_blank" rel="noopener noreferrer" href="https://github.com/daancsc/DaanChoose-Flask"><strong>Github</strong></a><br>
         {{ new Date().getFullYear() }} - <a target="_blank" rel="noopener noreferrer" href="https://dacsc.club"><strong>DAAN Computer Study Club</strong></a>
       </v-card-text>
     </v-card>
