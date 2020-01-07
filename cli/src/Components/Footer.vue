5a7acc488501254fc1ba9444cc0d8ae8d29856b3
William Chang <wilicw1346@gmail.com>
2020-01-07 19:33:45 +0800

    Modify footer information

--- cli/src/Components/Footer.vue
+++ cli/src/Components/Footer.vue
@@ -3,7 +3,8 @@
   <v-footer class="text-center pa-0 ma-0">
     <v-card flat tile color="#fafafa" width="100vw" class="text-center">
       <v-card-text class="orange--text">
-        {{ new Date().getFullYear() }} - <a target="_blank" rel="noopener noreferrer" href="https://dacsc.club" class="orange--text"><strong>DAAN Computer Study Club</strong></a>
+        Made by <a target="_blank" rel="noopener noreferrer" href="https://github.com/daancsc/DaanChoose-Flask">@wilicw</a>. Source Code on <a target="_blank" rel="noopener noreferrer" href="https://github.com/daancsc/DaanChoose-Flask"><strong>Github</strong></a><br>
+        {{ new Date().getFullYear() }} - <a target="_blank" rel="noopener noreferrer" href="https://dacsc.club"><strong>DAAN Computer Study Club</strong></a>
       </v-card-text>
     </v-card>
   </v-footer>
